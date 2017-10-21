c386bb973109019c761f9ebb1084a8a468a5b3f0
ClaireGizard <claire.1817@hotmail.fr>
2017-10-21 17:42:32 +0300

    modif html/css

--- labwork-5/src/pages/about/about.ts
+++ labwork-5/src/pages/about/about.ts
@@ -16,21 +16,8 @@ export class AboutPage {
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
   
-  public gotoHome(){
-    this.navCtrl.push('HomePage');
-  }
- 
-  public goToDoList(){
-    this.navCtrl.push('TodolistPage');
-}
- 
-  public goAbout(){
-    this.navCtrl.push('AboutPage');
-  }
-
   ionViewDidLoad() {
     console.log('ionViewDidLoad AboutPage');
   }
-
 }
 
